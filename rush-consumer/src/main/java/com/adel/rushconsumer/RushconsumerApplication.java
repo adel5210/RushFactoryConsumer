f1ae8bcc2cb2ed2e6d3d903d8c7f21906a95bcd4
adel <adel.albediwy@gmail.com>
2023-01-27 15:49:34 +0300

    Initial commit

---
+++ rush-consumer/src/main/java/com/adel/rushconsumer/RushconsumerApplication.java
@@ -0,0 +1,13 @@
+package com.adel.rushconsumer;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class RushconsumerApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(RushconsumerApplication.class, args);
+    }
+
+}
---
+++ rush-consumer/src/test/java/com/adel/rushconsumer/RushconsumerApplicationTests.java
@@ -0,0 +1,13 @@
+package com.adel.rushconsumer;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class RushconsumerApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
